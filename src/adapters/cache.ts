e54515803d33523dbc9ef003878b007661e8d0f9
lxfater <lxfater@gmail.com>
2023-11-19 21:23:01 +0800

    change url

--- src/adapters/cache.ts
+++ src/adapters/cache.ts
@@ -23,7 +23,7 @@ export async function ensureModel() {
     return loadModel()
   }
   const response = await fetch(
-    'https://cdn.jsdelivr.net/gh/lxfater/inpaint-web@main/model/migan.onnx'
+    'https://huggingface.co/lxfater/inpaint-web/resolve/main/migan.onnx'
   )
   const buffer = await response.arrayBuffer()
   await saveModel(buffer)
