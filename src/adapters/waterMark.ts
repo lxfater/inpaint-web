6ddb956d8f7a3c19a7fa122366739f7f13f8b365
N3oRay <serge.lopes.94@free.fr>
2024-04-25 19:13:25 +0200

    Update waterMark.ts

--- src/adapters/waterMark.ts
+++ src/adapters/waterMark.ts
@@ -5,6 +5,30 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import EnhancerWaterMark from 'watermark-enhancer'
 
+function processImage(
+  img: HTMLImageElement,
+  canvasId?: string
+): Promise<Uint8Array> {
+  return new Promise((resolve, reject) => {
+    try {
+      const src = cv.imread(img)
+      // eslint-disable-next-line camelcase
+      const src_rgb = new cv.Mat()
+      // 将图像从RGBA转换为RGB
+      cv.cvtColor(src, src_rgb, cv.COLOR_RGBA2RGB)
+      if (canvasId) {
+        cv.imshow(canvasId, src_rgb)
+      }
+      resolve(imgProcess(src_rgb))
+
+      src.delete()
+      src_rgb.delete()
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
 function imageDataToDataURL(imageFile: imageFile) {
   // 创建 canvas
   const canvas = document.createElement('canvas')
@@ -22,7 +46,21 @@ export default async function waterMark(
   imageFile: File | HTMLImageElement,
   callback: (progress: number) => void
 ) {
-  console.time('sessionCreate')
+  console.timeEnd('sessionCreate')
+
+  const img =
+    imageFile instanceof HTMLImageElement
+      ? imageFile
+      : await loadImage(URL.createObjectURL(imageFile))
+  const imageTersorData = await processImage(img)
+  const imageTensor = new ort.Tensor('float32', imageTersorData, [
+    1,
+    3,
+    img.height,
+    img.width,
+  ])
+  
+    console.time('sessionCreate')
   const result = await EnhancerWaterMark(
     {
       width: '100',
@@ -38,68 +76,8 @@ export default async function waterMark(
     }
   )(imageFile)
   console.log(imageFile, 'imageFile')
-console.timeEnd('sessionCreate')
-
-  const img =
-    imageFile instanceof HTMLImageElement
-      ? imageFile
-      : await loadImage(URL.createObjectURL(imageFile))
-  const imageTersorData = await processImage(img)
-  const imageTensor = new ort.Tensor('float32', imageTersorData, [
-    1,
-    3,
-    img.height,
-    img.width,
-  ])
-
-  // const result = await tileProc(imageTensor, model, callback)
-  console.time('postProcess')
-  const outsTensor = result
-  const chwToHwcData = postProcess(
-    outsTensor.data,
-    img.width * 4,
-    img.height * 4
-  )
-  const imageData = new ImageData(
-    new Uint8ClampedArray(chwToHwcData),
-    img.width * 4,
-    img.height * 4
-  )
-  console.log(imageData, 'imageData')
-  const url = imageDataToDataURL(imageData)
-  console.timeEnd('postProcess')
-
-  return url
-}
-
-export default async function superResolution(
-  imageFile: File | HTMLImageElement,
-  callback: (progress: number) => void
-) {
-  console.time('sessionCreate')
-  if (!model) {
-    const capabilities = await getCapabilities()
-    configEnv(capabilities)
-    const modelBuffer = await ensureModel('superResolution')
-    model = await ort.InferenceSession.create(modelBuffer, {
-      executionProviders: [capabilities.webgpu ? 'webgpu' : 'wasm'],
-    })
-  }
   console.timeEnd('sessionCreate')
-
-  const img =
-    imageFile instanceof HTMLImageElement
-      ? imageFile
-      : await loadImage(URL.createObjectURL(imageFile))
-  const imageTersorData = await processImage(img)
-  const imageTensor = new ort.Tensor('float32', imageTersorData, [
-    1,
-    3,
-    img.height,
-    img.width,
-  ])
-
-  const result = await tileProc(imageTensor, model, callback)
+  
   console.time('postProcess')
   const outsTensor = result
   const chwToHwcData = postProcess(
