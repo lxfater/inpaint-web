d5cac07b8726655d1985232b511c2c9f362dca76
N3oRay <serge.lopes.94@free.fr>
2024-04-25 17:21:25 +0200

    Update waterMark.ts

--- src/adapters/waterMark.ts
+++ src/adapters/waterMark.ts
@@ -59,7 +59,7 @@ function imageDataToDataURL(imageFile: imageFile) {
 
   // 绘制 imageFile 到 canvas
   const ctx = canvas.getContext('2d')
-  ctx.putImageData(imageFile.target.getAttribute('data-value'), 0, 0)
+  ctx.putImageData(imageFile.getData(), 0, 0)
 
   // 导出为数据 URL
   return canvas.toDataURL()
@@ -85,7 +85,8 @@ export default async function waterMark(
   )(imageFile)
   console.log(imageFile, 'imageFile')
   // const url = imageFile.getUrl()
-  console.log(imageFile.target.getAttribute('data-value'))
+  //console.log(imageFile.target.getAttribute('data-value'))
+  console.log(imageFile.getData())
   const url = imageDataToDataURL(imageFile)
   console.timeEnd('postProcess')
 
