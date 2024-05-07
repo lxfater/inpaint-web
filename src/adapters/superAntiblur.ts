cd2075c71243b42fb962002aad789dba8240766d
N3oRay <serge.lopes.94@free.fr>
2024-05-07 23:07:54 +0200

    Update superAntiblur.ts

--- src/adapters/superAntiblur.ts
+++ src/adapters/superAntiblur.ts
@@ -270,7 +270,7 @@ export default async function superAntiBlur(
   if (!model) {
     const capabilities = await getCapabilities()
     configEnv(capabilities)
-    const modelBuffer = await ensureModel('superPhi')
+    const modelBuffer = await ensureModel('superResolution')
     model = await ort.InferenceSession.create(modelBuffer, {
       executionProviders: [capabilities.webgpu ? 'webgpu' : 'wasm'],
     })
