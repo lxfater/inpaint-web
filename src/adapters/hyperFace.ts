66d74be38b34f069e5312a815832a880ffa8a3b8
N3oRay <serge.lopes.94@free.fr>
2024-05-21 20:30:14 +0200

    Update hyperFace.ts

--- src/adapters/hyperFace.ts
+++ src/adapters/hyperFace.ts
@@ -259,7 +259,7 @@ export default async function superFace(
       : await loadImage(URL.createObjectURL(imageFile))
 
   const imageTersorData = await processImage(img)
-  const imageTensor = new ort.Tensor('float16', imageTersorData, [
+  const imageTensor = new ort.Tensor('Uint16Array', imageTersorData, [
     1,
     3,
     img.height,
