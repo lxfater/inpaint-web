f495c24e3cb1ffca8b22df8edcc4ed43f12b647b
N3oRay <serge.lopes.94@free.fr>
2024-05-23 12:59:04 +0200

    Update occluderFace.ts

--- src/adapters/occluderFace.ts
+++ src/adapters/occluderFace.ts
@@ -74,8 +74,8 @@ async function tileProc(
   const outGOffset = outImageW * outImageH
   const outBOffset = outImageW * outImageH * 2
 
-  const tileSize = outImageH / 2 // 512
-  const tilePadding = outImageH * 0.25 // 128   // 512 * 0.25
+  const tileSize = imageH / 2 // 512
+  const tilePadding = imageH * 0.25 // 128   // 512 * 0.25
   const tileSizePre = tileSize - tilePadding * 2
 
   const tilesx = Math.ceil(inputDims[3] / tileSizePre)
