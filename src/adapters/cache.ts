ce69ab1a8e7ce5c8c5fa301b0a21359dfc6551e6
N3oRay <serge.lopes.94@free.fr>
2024-05-24 11:28:42 +0200

    Update cache.ts

--- src/adapters/cache.ts
+++ src/adapters/cache.ts
@@ -33,6 +33,22 @@ function getModel(modelType: modelType) {
     const currentModel = modelList[2]
     return currentModel
   }
+  /*
+  2xHFA2kAVCSRFormer_light
+
+  Name: 2xHFA2kAVCSRFormer_light
+  License: CC BY 4.0
+  Network: SRFormer_light
+  Scale: 2
+  Purpose: 2x anime upscaling model that handles AVC (h264) compression
+  Iterations: 140000
+  batch_size: 2-4
+  HR_size: 128-192
+  Dataset: HFA2k_h264
+  Number of train images: 2568
+  OTF Training: No
+  Pretrained_Model_G: SRFormerLight_SRx2_DIV2K.pth
+    */
   // mediumResolution - https://github.com/Phhofm/models/blob/main/2xHFA2kAVCSRFormer_light/onnx/2xHFA2kAVCSRFormer_light_16_onnxsim_fp32.onnx
     if (modelType === 'mediumResolution') {
     const modelList = [
